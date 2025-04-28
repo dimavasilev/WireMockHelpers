76779cb563b3c8ebfc3bf28f7c1b5be787dbfedb
Dima <likgooo@mail.ru>
2025-04-28 21:49:27 +0300

    чистка кода

--- src/test/java/wiremock/RestApi_Test.java
+++ src/test/java/wiremock/RestApi_Test.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 
 @WireMockTest()
-public class RestAPI_Test {
+public class RestApi_Test {
     @BeforeClass
     public static void register() throws IOException {
         new RegisterStubsFile().registerStubs(System.getProperty("user.dir") + "/src/test/resources/user_stub.json", "/user");
